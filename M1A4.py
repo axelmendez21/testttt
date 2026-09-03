240e93815c328e3c88d6e4eb989c3319464bc787
axelmendez21 <axel.mendez21@csu.fullerton.edu>
2026-09-03 15:36:29 +0000

    Finished with module 1 Assignment 4

---
+++ M1A4.py
@@ -0,0 +1,10 @@
+# Name: Axel Mendez
+# Student ID: 882349616
+# Section: CPSC 223P-07
+# Assignment: Module 1 Assignment 4
+
+catchphrase_str = str(input("Please enter a catchphrase: "))
+print(f"[{catchphrase_str.upper()}]")
+print(f"[{catchphrase_str.lower()}]")
+print(f"[{catchphrase_str.lstrip()}]")
+print(f"[{catchphrase_str.rstrip()}]")
