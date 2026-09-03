ff9778700f2439fe3b9e160b3c8cd6b96189b993
axelmendez21 <axel.mendez21@csu.fullerton.edu>
2026-09-03 15:15:36 +0000

    Module 1 Assignment 1

---
+++ M1A1.py
@@ -0,0 +1,6 @@
+# Name: Axel Mendez
+# Student ID: 882349616
+# Section: CPSC 223P-07
+# Assignment: Module 1 Assignment 1
+
+print("It's a me, Mario!")
---
+++ blank.py
@@ -0,0 +1,12 @@
+#! /usr/local/python/3.12.1/bin/python
+# Name: Axel Mendez
+# Student ID: 882346616
+# Section: #7
+# Assignment: Module 3 Assignment 1
+
+x,y,z = 1,2,3
+print(f"x is {x} and y is {y} and z is {z}")
+
+print("What is your name? ")
+name = input("")
+print(f"Your name is {name}")
