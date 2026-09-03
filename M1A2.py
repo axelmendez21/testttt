ce46bc64bde9a02736d6d58a5c814d94dce7fe9d
axelmendez21 <axel.mendez21@csu.fullerton.edu>
2026-09-03 15:29:16 +0000

    Completed Module 1 Assignment 3

--- M1A2.py
+++
@@ -1,7 +0,0 @@
-# Name: Axel Mendez
-# Student ID: 882349616
-# Section: CPSC 223P-07
-# Assignment: Module 1 Assignment 2
-
-name_str = input("What is your name? ")
-print(f"It's dangerous to go alone {name_str}, take this!")
---
+++ M1A3.py
@@ -0,0 +1,15 @@
+# Name: Axel Mendez
+# Student ID: 882349616
+# Section: CPSC 223P-07
+# Assignment: Module 1 Assignment 2
+
+first_int = int(input("What is the first integer? "))
+second_float = float(input("What is the second float? "))
+message_str = str(f"{first_int} + {second_float} is {first_int + second_float}")
+print(message_str)
+message_str = str(f"{first_int} - {second_float} is {first_int - second_float}")
+print(message_str)
+message_str = str(f"{first_int} * {second_float} is {first_int * second_float}")
+print(message_str)
+message_str = str(f"{first_int} / {second_float} is {first_int / second_float}")
+print(message_str)
