af01fa1cccb54cdc08a0cb78eee7fb4c6e23f2b4
axelmendez21 <axel.mendez21@csu.fullerton.edu>
2026-09-03 15:30:22 +0000

    fix

--- M1A3.py
+++ M1A3.py
@@ -1,7 +1,7 @@
 # Name: Axel Mendez
 # Student ID: 882349616
 # Section: CPSC 223P-07
-# Assignment: Module 1 Assignment 2
+# Assignment: Module 1 Assignment 3
 
 first_int = int(input("What is the first integer? "))
 second_float = float(input("What is the second float? "))
